31dccba16bd0ba766fe5ecd25a62647097a9a3bb
Yolandalida <90179473+Yolandalida@users.noreply.github.com>
2023-04-21 23:04:43 +0800

    Update DebugException.h

--- npc/exceptions/DebugException.h
+++ npc/exceptions/DebugException.h
@@ -4,11 +4,11 @@
 #include <string>
 #define NOW_FUNC_NAME std::string(__func__) + ": "
 
-namespace pascal
+namespace Pascal
 {
 class DebugException : public std::runtime_error
 {
 public:
     DebugException(std::string msg) : std::runtime_error("DebugException " + msg) {}
 };
-} // namespace pascal
+} // namespace Pascal
