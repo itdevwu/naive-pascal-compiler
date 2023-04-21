9d5e7726d958d7446fbff1194c0a0ea727d5a980
Yolandalida <90179473+Yolandalida@users.noreply.github.com>
2023-04-21 23:05:33 +0800

    Update ProcedureNotFoundException.h

--- npc/exceptions/ProcedureNotFoundException.h
+++ npc/exceptions/ProcedureNotFoundException.h
@@ -3,11 +3,11 @@
 #include <stdexcept>
 #include <string>
 
-namespace pascal
+namespace Pascal
 {
     class ProcedureNotFoundException : public std::runtime_error
     {
     public:
         ProcedureNotFoundException(std::string name) : std::runtime_error("Procedure not found: " + name + ".") {}
     };
-} // namespace pascal
+} // namespace Pascal
